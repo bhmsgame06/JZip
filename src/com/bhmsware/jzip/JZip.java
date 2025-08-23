2e2b8fbbe734cf09c3051243343c4d1623c3a2fe
bhmsgame06 <bhmsgamexbox2010@gmail.com>
2025-08-24 00:11:49 +0500

    Archive reading order updated
    You can read an EOCD only once by initializing JZip object

--- src/com/bhmsware/jzip/JZip.java
+++ src/com/bhmsware/jzip/JZip.java
@@ -2,128 +2,147 @@ package com.bhmsware.jzip;
 
 import java.io.*;
 
-public final class JZip {
-	private static final int MAX_VERSION_SUPPORTED = 20;
-	private static final int[] SUPPORTED_COMPRESSION_METHODS = {
-		0, 8
-	};
+public class JZip {
+	public static final int[] SUPPORTED_COMPRESSION_METHODS = { 0, 8 };
 
-	private JZip() {
+	protected DataInputStream inStream;
+	protected int numCD;
+	protected int offsetCD;
+
+	public JZip(InputStream is) throws IOException {
+		inStream = new DataInputStream(is);
+		int size = inStream.available();
+
+		int skip = size - 22;
+		byte[] buffer = new byte[256];
+
+		// searching for EOCD
+		do {
+			if((skip -= 234) < 0) skip = 0;
+
+			inStream.mark(0);
+			inStream.skip(skip);
+			inStream.read(buffer, 0, size >= 256 ? 256 : size);
+			inStream.reset();
+
+			// yanderedev moment yah
+			for(int i = 238; i > 3;) {
+
+				if(buffer[--i] == 0x06) {
+					if(buffer[--i] == 0x05) {
+						if(buffer[--i] == 'K') {
+							if(buffer[--i] == 'P') {
+								readEndOfCentralDirectory(buffer, i);
+								return;
+							}
+						}
+					}
+				}
+
+			}
+		} while(skip > 0);
+
+		throw new IllegalArgumentException("JZip: EOCD not found");
 	}
 
-	public static byte[] extractFile(InputStream is, String path) throws Exception {
-		DataInputStream data = new DataInputStream(is);
+	protected void readEndOfCentralDirectory(byte[] buffer, int skipToEOCD) {
+		// signature (4 bytes)
+		// disk num (2 bytes)
+		// start disk num (2 bytes)
+		// CD num in disk (2 bytes)
+		numCD = (buffer[skipToEOCD + 8] & 0xff) | 
+			((buffer[skipToEOCD + 9] & 0xff) << 8);
+		// total CD num (2 bytes)
+		// CD size (4 bytes)
+		// CD offset (4 bytes)
+		offsetCD = (buffer[skipToEOCD + 16] & 0xff) | 
+			((buffer[skipToEOCD + 17] & 0xff) << 8) | 
+			((buffer[skipToEOCD + 18] & 0xff) << 16) | 
+			((buffer[skipToEOCD + 19] & 0xff) << 24);
+		// comment length (2 bytes)
+		// comment
+	}
 
-		int versionToExtract,
-			compressionMethod,
-			filenameLength,
-			extraFieldLength,
-			fileCommentLength;
+	public byte[] extractFile(String path) throws Exception {
 		byte compressed[];
 		byte uncompressed[];
 
-		try {
-			while(true) {
-				int signature = data.readInt();
-				switch(signature) {
-					// Central Directory header
-					case 0x504b0102:
-						// version made (1 bytes)
-						// OS made (1 bytes)
-						// version to extract (1 bytes)
-						// OS to extract (1 bytes)
-						// bit flag (2 bytes)
-						// compression method (2 bytes)
-						// mod time (2 bytes)
-						// mod date (2 bytes)
-						// CRC-32 (4 bytes)
-						// compressed size (4 bytes)
-						// uncompressed size (4 bytes)
-						data.skip(24);
+		inStream.mark(0);
+		inStream.skip(offsetCD);
+		for(int i = 0; i < numCD; i++) {
+			// CD signature
+			if(inStream.readInt() != 0x504b0102)
+				throw new IllegalArgumentException("JZip: " + path + ": Bad CD signature");
+			
+			byte[] structCD = new byte[42];
+
+			inStream.read(structCD);
+
+			// reading filename first
+			byte[] bFilename = new byte[(structCD[24] & 0xff) | 
+				((structCD[25] & 0xff) << 8)];
+			inStream.read(bFilename);
+
+			if(new String(bFilename).equals(path)) {
+
+				// check compression method
+				int compMethod = (structCD[6] & 0xff) | ((structCD[7] & 0xff) << 8);
+				if(!checkCompMethod(compMethod))
+					throw new IllegalArgumentException("JZip: " + path + ": Unsupported compression method");
+
+				// allocate memory
+				compressed = new byte[(structCD[16] & 0xff) | 
+					((structCD[17] & 0xff) << 8) | 
+					((structCD[18] & 0xff) << 16) | 
+					((structCD[19] & 0xff) << 24)];
+				uncompressed = new byte[(structCD[20] & 0xff) | 
+					((structCD[21] & 0xff) << 8) | 
+					((structCD[22] & 0xff) << 16) | 
+					((structCD[23] & 0xff) << 24)];
+				
+				// LFH
+				inStream.reset();
+				inStream.mark(0);
+				// offset to LFH
+				inStream.skip(((structCD[38] & 0xff) | 
+							((structCD[39] & 0xff) << 8) | 
+							((structCD[40] & 0xff) << 16) | 
+							((structCD[41] & 0xff) << 24)));
+				// LFH signature
+				if(inStream.readInt() != 0x504b0304)
+					throw new IllegalArgumentException("JZip: " + path + ": Bad LFH signature");
+				// skip useless LFH stuff
+				inStream.skip(22);
+				inStream.skip(
 						// filename length
-						filenameLength = data.readUnsignedByte() | (data.readUnsignedByte() << 8);
+						(inStream.readUnsignedByte() | 
+						 (inStream.readUnsignedByte() << 8)) + 
 						// extra field length
-						extraFieldLength = data.readUnsignedByte() | (data.readUnsignedByte() << 8);
-						// comment length
-						fileCommentLength = data.readUnsignedByte() | (data.readUnsignedByte() << 8);
-						// disk num (2 bytes)
-						// int. file attributes (2 bytes)
-						// ext. file attributes (4 bytes)
-						// Local File Header offset (4 bytes)
-						// filename
-						// extra field data
-						// file comment
-						data.skip(12 + filenameLength + extraFieldLength + fileCommentLength);
+						(inStream.readUnsignedByte() | 
+						 (inStream.readUnsignedByte() << 8)));
+				inStream.read(compressed);
+				inStream.reset();
 
-						break;
-		
-					// Local File Header
-					case 0x504b0304:
-						// version to extract
-						versionToExtract = data.readUnsignedByte();
-						if(versionToExtract > MAX_VERSION_SUPPORTED) {
-							throw new IllegalArgumentException("ZIP: extract " + path + " failed: " + "maximum version " + MAX_VERSION_SUPPORTED + " is only supported, but " + versionToExtract + " found");
-						}
-						// OS to extract (1 byte)
-						// bit flag (2 bytes)
-						data.skip(3);
-						// compression method
-						compressionMethod = data.readUnsignedByte() | (data.readUnsignedByte() << 8);
-						if(!checkCompMethod(compressionMethod)) {
-							throw new IllegalArgumentException("ZIP: extract " + path + " failed: " + "unsupported compression method " + compressionMethod);
-						}
-						// mod time (2 bytes)
-						// mod date (2 bytes)
-						// CRC-32 (4 bytes)
-						data.skip(8);
-						// compressed size
-						compressed = new byte[data.readUnsignedByte() | (data.readUnsignedByte() << 8) | (data.readUnsignedByte() << 16) | (data.readUnsignedByte() << 24)];
-						// uncompressed size
-						uncompressed = new byte[data.readUnsignedByte() | (data.readUnsignedByte() << 8) | (data.readUnsignedByte() << 16) | (data.readUnsignedByte() << 24)];
-						// filename length
-						filenameLength = data.readUnsignedByte() | (data.readUnsignedByte() << 8);
-						// extra field length
-						extraFieldLength = data.readUnsignedByte() | (data.readUnsignedByte() << 8);
-						// filename
-						byte[] filename = new byte[filenameLength];
-						data.read(filename);
-						// extra field
-						data.skip(extraFieldLength);
-						// data decoding
-						data.read(compressed);
-						if(path.equals(new String(filename))) {
-							if(!decodeData(uncompressed, compressed, compressionMethod)) {
-								throw new IllegalArgumentException("ZIP: extract " + path + " failed: " + "invalid compressed data to inflate");
-							}
-							return uncompressed;
-						}
-		
-						break;
+				// decode now
+				if(!decodeData(uncompressed, compressed, compMethod))
+					throw new IllegalArgumentException("JZip: " + path + ": invalid compressed data to inflate");
 
-					// End Of Central Directory Record
-					case 0x504b0506:
-						// disk num (2 bytes)
-						// start disk num (2 bytes)
-						// CD num in disk (2 bytes)
-						// total CD num (2 bytes)
-						// CD size (4 bytes)
-						// CD offset (4 bytes)
-						// comment length (2 bytes)
-						// comment
-						data.skip(16 + (data.readUnsignedByte() | (data.readUnsignedByte() << 8)));
+				return uncompressed;
 
-						break;
+			} else {
 
-					default:
-						throw new IllegalArgumentException("ZIP: extract " + path + " failed: " + "invalid signature " + Integer.toHexString(signature));
-				}
+				// not our entry we're looking for
+				inStream.skip((structCD[26] & 0xff) | ((structCD[27] & 0xff) << 8));
+				inStream.skip((structCD[28] & 0xff) | ((structCD[29] & 0xff) << 8));
+
 			}
-		} catch(EOFException e) {
-			throw new EOFException("ZIP: extract " + path + " failed: " + "file not found");
 		}
+
+		// don't forget to put your file inside next time
+		throw new IllegalArgumentException("JZip: " + path + ": entry not found");
 	}
 	
-	private static boolean decodeData(byte[] out, byte[] in, int compressionMethod) {
+	public static boolean decodeData(byte[] out, byte[] in, int compressionMethod) {
 		switch(compressionMethod) {
 			// ZIP_CM_STORE
 			case 0:
@@ -138,7 +157,7 @@ public final class JZip {
 		return false;
 	}
 
-	private static boolean checkCompMethod(int comp) {
+	public static boolean checkCompMethod(int comp) {
 		for(int i = 0; i < SUPPORTED_COMPRESSION_METHODS.length; i++) {
 			if(comp == SUPPORTED_COMPRESSION_METHODS[i]) return true;
 		}

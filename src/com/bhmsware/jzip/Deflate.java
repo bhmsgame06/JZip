104b4cd4b2c030829c79e98bf4fff32f8edf7d0e
bhmsgame06 <bhmsgamexbox2010@gmail.com>
2025-08-24 00:25:18 +0500

    And optimized for a constrained memory (kinda)

--- src/com/bhmsware/jzip/Deflate.java
+++ src/com/bhmsware/jzip/Deflate.java
@@ -2,25 +2,25 @@ package com.bhmsware.jzip;
 
 public class Deflate {
 	// tables
-	public static final int LENGTH_TABLE[][] = {
+	public static final short LENGTH_TABLE[][] = {
 		// length
 		{3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 19, 23, 27, 31, 35, 43, 51, 59, 67, 83, 99, 115, 131, 163, 195, 227, 258},
 		// extra bits
 		{0, 0, 0, 0, 0, 0, 0, 0,  1,  1,  1,  1,  2,  2,  2,  2,  3,  3,  3,  3,  4,  4,  4,  4,   5,   5,   5,   5,   0  }
 	};
-	public static final int DISTANCE_TABLE[][] = {
+	public static final short DISTANCE_TABLE[][] = {
 		// distance
 		{1, 2, 3, 4, 5, 7, 9, 13, 17, 25, 33, 49, 65, 97, 129, 193, 257, 385, 513, 769, 1025, 1537, 2049, 3073, 4097, 6145, 8193, 12289, 16385, 24577},
 		// extra bits
 		{0, 0, 0, 0, 1, 1, 2, 2,  3,  3,  4,  4,  5,  5,  6,   6,   7,   7,   8,   8,   9,    9,    10,   10,   11,   11,   12,   12,    13,    13   }
 	};
-	public static final int CODE_LENGTH_ALPHABET[] = {
+	public static final byte CODE_LENGTH_ALPHABET[] = {
 		16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15
 	};
-	public static final int FIXED_CODES[];
-	public static final int FIXED_CODE_LENGTHS[];
-	public static final int FIXED_DIST_CODES[];
-	public static final int FIXED_DIST_CODE_LENGTHS[];
+	public static final short FIXED_CODES[];
+	public static final byte FIXED_CODE_LENGTHS[];
+	public static final short FIXED_DIST_CODES[];
+	public static final byte FIXED_DIST_CODE_LENGTHS[];
 	// buffers
 	private byte[] out;
 	private byte[] in;
@@ -32,10 +32,10 @@ public class Deflate {
 	private int outIndex = 0;
 
 	static {
-		int[] fixedCodes = new int[288];
-		int[] fixedCodeLen = new int[288];
-		int[] fixedDistCodes = new int[30];
-		int[] fixedDistCodeLen = new int[30];
+		short[] fixedCodes = new short[288];
+		byte[] fixedCodeLen = new byte[288];
+		short[] fixedDistCodes = new short[30];
+		byte[] fixedDistCodeLen = new byte[30];
 		
 		// fixed literal codes
 		for(int i = 0; i < 144; i++)
@@ -68,9 +68,9 @@ public class Deflate {
 		this.in = in;
 	}
 
-	private static void generateCodes(int codes[], int codeLen[]) {
-		int blCount[] = new int[16];
-		int nextCode[] = new int[16];
+	private static void generateCodes(short codes[], byte codeLen[]) {
+		short blCount[] = new short[16];
+		short nextCode[] = new short[16];
 		int maxBits = 0;
 
 		// setting up blCount
@@ -83,7 +83,7 @@ public class Deflate {
 		// setting up nextCode
 		for(int code = 0, bits = 1; bits <= maxBits; bits++) {
 			code = (code + blCount[bits - 1]) << 1;
-			nextCode[bits] = code;
+			nextCode[bits] = (short)code;
 		}
 
 		// generate the tree itself
@@ -94,26 +94,25 @@ public class Deflate {
 		}
 	}
 
-	private int decodeNextValue(int codes[], int codeLen[]) {
-		int workingCodeLen[] = new int[codeLen.length];
-		System.arraycopy(codeLen, 0, workingCodeLen, 0, workingCodeLen.length);
+	private int decodeNextValue(short codes[], byte codeLen[], byte tempCodeLen[]) {
+		System.arraycopy(codeLen, 0, tempCodeLen, 0, tempCodeLen.length);
 
 		int bitIndex = 0;
 		while(true) {
 			int bitRead = -1;
 
-			for(int i = 0; i < workingCodeLen.length; i++) {
+			for(int i = 0; i < tempCodeLen.length; i++) {
 
-				if(workingCodeLen[i] > 0) {
+				if(tempCodeLen[i] > 0) {
 
-					int shift = workingCodeLen[i] - 1 - bitIndex;
+					int shift = tempCodeLen[i] - 1 - bitIndex;
 					if(shift < 0) {
 						return i;
 					}
 
 					if(bitRead < 0) bitRead = readBit();
 					if(((codes[i] >> shift) & 1) != bitRead) {
-						workingCodeLen[i] = 0;
+						tempCodeLen[i] = 0;
 					}
 
 				}
@@ -131,10 +130,10 @@ public class Deflate {
 		// decode cycle for each block
 		do {
 			// huffman related
-			int codes[] = null;
-			int codeLen[] = null;
-			int distCodes[] = null;
-			int distCodeLen[] = null;
+			short codes[] = null;
+			byte codeLen[] = null;
+			short distCodes[] = null;
+			byte distCodeLen[] = null;
 
 			// header of this block
 			BFINAL = readBit() == 1;
@@ -173,26 +172,27 @@ public class Deflate {
 					int HDIST = 1 + readConst(5);
 					int HCLEN = 4 + readConst(4);
 
-					int codeLen4cl[] = new int[19];
-					int codes4cl[] = new int[19];
+					byte codeLen4cl[] = new byte[19];
+					short codes4cl[] = new short[19];
 
 					for(int i = 0; i < HCLEN; i++) {
-						codeLen4cl[CODE_LENGTH_ALPHABET[i]] = readConst(3);
+						codeLen4cl[CODE_LENGTH_ALPHABET[i]] = (byte)readConst(3);
 					}
 
 					// generate codes for code lengths
 					generateCodes(codes4cl, codeLen4cl);
 
-					codes = new int[288];
-					codeLen = new int[288];
-					distCodes = new int[30];
-					distCodeLen = new int[30];
+					codes = new short[288];
+					codeLen = new byte[288];
+					distCodes = new short[30];
+					distCodeLen = new byte[30];
 
-					int currentCodes[] = codes;
-					int currentCodeLen[] = codeLen;
+					short currentCodes[] = codes;
+					byte currentCodeLen[] = codeLen;
 					int count = HLIT;
 
 					// decode code lengths => setting up literal code lengths, then distance code lengths
+					byte tempCodeLen4cl[] = new byte[19];
 					for(int p = 0; p < 2; p++) {
 
 						if(p > 0) {
@@ -202,14 +202,14 @@ public class Deflate {
 						}
 
 						for(int i = 0; i < count;) {
-							int value = decodeNextValue(codes4cl, codeLen4cl);
+							int value = decodeNextValue(codes4cl, codeLen4cl, tempCodeLen4cl);
 
 							switch(value) {
 								case 16:
 									int target = i + (3 + readConst(2));
 									int prev = currentCodeLen[i - 1];
 									for(; i < target; i++)
-										currentCodeLen[i] = prev;
+										currentCodeLen[i] = (byte)prev;
 									break;
 
 								case 17:
@@ -221,7 +221,7 @@ public class Deflate {
 									break;
 
 								default:
-									currentCodeLen[i++] = value;
+									currentCodeLen[i++] = (byte)value;
 							}
 						}
 
@@ -241,8 +241,10 @@ public class Deflate {
 
 			if(BTYPE != 0) {
 				// value decoding
+				byte tempCodeLen[] = new byte[288];
+				byte tempDistCodeLen[] = new byte[30];
 				int value;
-				while((value = decodeNextValue(codes, codeLen)) != 256) {
+				while((value = decodeNextValue(codes, codeLen, tempCodeLen)) != 256) {
 					if(value < 256) {
 
 						// literal byte
@@ -255,7 +257,7 @@ public class Deflate {
 						// target (got from length)
 						int target = outIndex + (LENGTH_TABLE[0][value - 257] + readConst(LENGTH_TABLE[1][value - 257]));
 						// distance
-						int dist = decodeNextValue(distCodes, distCodeLen);
+						int dist = decodeNextValue(distCodes, distCodeLen, tempDistCodeLen);
 						dist = DISTANCE_TABLE[0][dist] + readConst(DISTANCE_TABLE[1][dist]);
 
 						// copy
